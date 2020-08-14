800467ef1b7b1291f3e05e43f62232942cfa6081
slavapoltoranin <61622614+slavapoltoranin@users.noreply.github.com>
2020-08-14 21:08:55 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.decision__carousel').slick({
+        // dots: true,
+        speed: 1000,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icon/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icon/right.png"></button>'
+    });
+});
